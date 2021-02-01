d66d1af23be903c3675134be7e37f6c289043de4
Gabriel <16545056+gary17@users.noreply.github.com>
2021-02-01 15:33:19 +0100

    Factor out 'which' into a distinct command

--- shell-run.swift
+++ shell-run.swift
@@ -3,23 +3,22 @@
 import Foundation
 
 class ShCmd {
-	enum RunError: Error { case commandNotFound(command: String) }
+	enum RunError: Error { case systemError, commandNotFound(command: String) }
 
 	typealias RunResult = (stdout: String?, stderr: String?, rc: /* Process.terminationStatus */ Int32)
 
-	// FYI: Process API uses optionals vs. empty collection instances; follow
+	// Process API uses optionals vs. empty collection instances; follow
 	init(path: String, args: [String]? = nil, env: [String : String]? = nil) {
-		// will spawn a subprocess
-		process.executableURL = URL(fileURLWithPath: path)
+		process.executableURL = URL(fileURLWithPath: path) // will spawn a subprocess
+		
 		process.arguments = args
-
 		process.environment = env
 
-		process.standardOutput = pipes.stdout // FYI: a FileHandle or a Pipe
+		process.standardOutput = pipes.stdout // a FileHandle or a Pipe
 		process.standardError = pipes.stderr
 	}
 	
-	// FYI: potentially multiple points of failure, prefer a throwing over nullable initializer
+	// potentially multiple points of failure; prefer a throwing over nullable initializer
 	convenience init(_ command: String, _ args: [String]? = nil, env: [String : String]? = nil,
 		usePathCache: Bool = true) throws {
 		
@@ -38,23 +37,14 @@ class ShCmd {
 
 		if usePathCache {
 			// prefer guaranteed local cache performance over unguaranteed /usr/bin/env behavior
-			if let hit = /* check-and-read */ ShCmd.pathCache[command] {
-				self.init(path: hit, args: args, env: env)
+			if let path = /* check-and-read */ ShCmd.pathCache[command] {
+				self.init(path: path, args: args, env: env)
 			}
 			else {
-				// the command: (/bin/sh -l -c "which ls") expands "ls" into "/bin/ls"
-				let cmd = ShCmd(path: Config.Tools.sh, args: ["-l", "-c", "which \(command)"])
-				let result = try cmd.run()
-				
-				// FIXME: verify the return code
+				let path = try ShCmd.which(command)
+				ShCmd.pathCache[command] = path // cache-in
 
-				guard let stdout = result.stdout else { throw RunError.commandNotFound(command: command) }
-				let lookup = stdout.trimmingCharacters(in: NSCharacterSet.whitespacesAndNewlines)
-
-				// FIXME: verify only one line was returned
-
-				ShCmd.pathCache[command] = lookup // cache-in
-				self.init(path: lookup, args: args, env: env)
+				self.init(path: path, args: args, env: env)
 			}
 		}
 		else {
@@ -99,9 +89,40 @@ extension ShCmd
 extension ShCmd.RunError: LocalizedError {
 	var errorDescription: String? {
 		switch self {
+			case .systemError:
+				return NSLocalizedString("unexpected system error", comment: "")
 			case .commandNotFound(let command):
 				return NSLocalizedString("command '\(command)' not found", comment: "")
 		}
+	}
+}
+
+extension ShCmd {
+	// special commands
+	static func which(_ command: String) throws -> String {
+		// the command: (/bin/sh -l -c "which ls") expands "ls" into "/bin/ls"
+		let cmd = ShCmd(path: Config.Tools.sh, args: ["-l", "-c", "which \(command)"])
+		
+		let result = try cmd.run()
+		
+		// WARNING: missing shell or missing (non- built-in) which might return 127
+		guard result.rc == 0, let stdout = result.stdout
+			else { throw RunError.commandNotFound(command: command) } // user error
+		
+		let path = stdout.trimmingCharacters(in: NSCharacterSet.whitespacesAndNewlines)
+		
+		// sanity
+		guard path.split(whereSeparator: \.isNewline).count == 1
+			else { throw RunError.systemError } // call devops
+
+		return path
+	}
+}
+
+extension ShCmd {
+	// wrap complex commands into argument-safe factories
+	static func lsLA() throws -> ShCmd {
+		return try ShCmd("ls", ["-l", "-a"]) // FIXME: a complex sample for: 'find . -name "*pattern*" -type f -ls', etc.
 	}
 }
 
@@ -119,9 +140,14 @@ do {
 		}
 
 		do {
+			let result = try ShCmd.lsLA().run()
+			if result.rc == 0, let stdout = result.stdout { print(stdout) }
+		}
+
+		do {
 			try ShCmd("ls65535").run()
 		}
-		catch {
+		catch { // intended, catch and carry on with subsequent tests
 			print(error.localizedDescription)
 		}
 
